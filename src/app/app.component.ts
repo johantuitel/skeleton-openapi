002c18a22102b245c2b6dc30f50fb0e80cd765e3
Johan Tuitel <jtuitel@hotmail.com>
2024-03-28 22:45:04 +0100

    implemented openapi specs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostsService } from './api/v1';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'skeleton-openapi';
+  posts$ = this.postService.getPosts();
+
+  constructor(private postService: PostsService) { }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { ApiModule, Configuration, ConfigurationParameters } from './api/v1';
+import { routes } from './app.routes';
+import { environment } from './environments/environment';
+
+export function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: environment.basePath,
+  };
+  return new Configuration(params);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(ApiModule.forRoot(apiConfigFactory))]
 };
